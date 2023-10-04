9e73fa555ac64233f461d95e9f2359498e1dc3ea
Masekela Lerato <leratomasekela234@gmail.com>
2023-10-04 19:07:12 +0200

    completed the challenge

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -6,20 +6,25 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   
-  //used a if and else statement to check whether either of the input fields (dividend or divider) is empty before performing the division. If either of them is empty, it displays the error message
+  //used a if and else statement to check whether either of the input fields (dividend or divider) are empty before performing the division. If either of them is empty, it displays the error message
   if(dividend === '' || divider === ''){
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
   }  
+  
   // added an else if statement that will throw an error when nin-numeric values are entered
-   if(isNaN(dividend) && isNaN(divider)){
-    display.error("Something critical went wrong. Please reload the page")
-    console.log('Invalid input')
+   else if(isNaN(dividend) && isNaN(divider)){
+    //This line of code sets the HTML content of the <body>
+    document.body.innerHTML = "Something critical went wrong. Please reload the page"
+    console.error('Invalid input')
   } 
+  
   // added an else if statement that will check if the diver is less than zero
   else if (divider < 0) {
     result.innerText = "Division not performed. Invalid number provided. Try again";
     console.error("Error: Invalid number provided");
-  } else{
+  } 
+  
+  else{
   //used math.floor to ensure that there are no decimal places in th output
   result.innerText = Math.floor(dividend / divider);
   }
